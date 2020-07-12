bfc25eef68d8bf0dcdda6f9fca755d2018fd941f
Christian Zelaya MACBOOK <christian@cerouno.com.py>
2020-07-12 18:18:25 -0400

    Ajuste 202007121810

--- include/menu_examen.php
+++ include/menu_examen.php
@@ -121,7 +121,6 @@
                 <!-- Sidebar navigation-->
                 <nav class="sidebar-nav">
                     <ul id="sidebarnav">
-<!--
                         <li class="sidebar-item">
                         	<a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
                         		<i class="icon-Car-Wheel"></i>
@@ -129,14 +128,14 @@
                         	</a>
 							<ul aria-expanded="false" class="collapse first-level">
                             	<li class="sidebar-item">
-                               		<a href="../public/home.php" class="sidebar-link">
+                               		<a href="javascript:void(0)" class="sidebar-link">
                                			<i class="mdi mdi-home"></i>
                                			<span class="hide-menu"> Home </span>
                                		</a>
                                	</li>
                             </ul>
                         </li>
--->
+
                         <li class="sidebar-item">
                         	<a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
                         		<i class="icon-Neutron"></i>
