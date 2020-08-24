cffd9ca9cc6b0d27e47d76aa921d009c7c8b9987
Christian Zelaya MACBOOK <christian@cerouno.com.py>
2020-08-24 12:12:03 -0400

    Ajuste 202008241105

---
+++ examen/preencuentro.php
@@ -0,0 +1,211 @@
+<?php
+    require '../class/function/curl_api.php';
+    require '../class/function/function.php';
+    require '../class/session/session_system.php';
+
+    if ($usu_05 != 9 && $usu_05 != 10 && $usu_05 != 11 && $usu_05 != 157){
+        header('Location: ../examen/home.php?code=401&msg=No tiene permiso para ingresar!Contacte con TI');
+    }
+
+    if(isset($_GET['competicion'])){
+        $valorCompeticion   = $_GET['competicion'];
+    } else {
+        $valorCompeticion   = 0;
+    }
+
+    if(isset($_GET['encuentro'])){
+        $valorEncuentro     = $_GET['encuentro'];
+    } else {
+        $valorEncuentro     = 0;
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+
+<head>
+<?php
+    include '../include/header.php';
+?>
+</head>
+
+<body>
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
+    <div class="preloader">
+        <div class="lds-ripple">
+            <div class="lds-pos"></div>
+            <div class="lds-pos"></div>
+        </div>
+    </div>
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <div id="main-wrapper">
+
+<?php
+    switch ($usu_05) {
+        case 157:
+            include '../include/menu_examen.php';
+            break;
+        
+        default:
+            include '../include/menu.php';
+            break;
+    }
+?>
+       
+        <!-- Page wrapper  -->
+        <!-- ============================================================== -->
+        <div class="page-wrapper">
+            <!-- ============================================================== -->
+            <!-- Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <div class="page-breadcrumb">
+                <div class="row">
+                    <div class="col-5 align-self-center">
+                        <h4 class="page-title"><?php echo $usu_02.' - '.$usu_01; ?></h4>
+                        <div class="d-flex align-items-center"></div>
+                    </div>
+                    <div class="col-7 align-self-center">
+                        <div class="d-flex no-block justify-content-end align-items-center">
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item">
+                                        <a href="../examen/home.php">HOME</a>
+                                    </li>
+                                    <li class="breadcrumb-item" aria-current="page">
+                                        <a href="../examen/competicion.php">COMPETICIONES</a>
+                                    </li>
+                                    <li class="breadcrumb-item" aria-current="page">
+                                        <a href="../examen/encuentro.php?competicion=<?php echo $valorCompeticion; ?>">ENCUENTROS</a>
+                                    </li>
+                                    <li class="breadcrumb-item active" aria-current="page">PRE-ENCUENTRO TEST</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+                <!-- ============================================================== -->
+                <!-- Start Page Content -->
+                <!-- ============================================================== -->
+                <!-- basic table -->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <h4 class="col-6 card-title">PRE-ENCUENTRO Test</h4>
+                                    <h4 class="col-6 card-title" style="text-align: right;">
+<?php
+    if ($usu_04 == 39393 && $usu_05 == 157){
+?>
+                                        <a class="btn btn-info" style="background-color:#005ea6; border-color:#005ea6;"  href="../examen/preencuentro_2_crud.php?competicion=<?php echo $valorCompeticion; ?>&encuentro=<?php echo $valorEncuentro; ?>&tipo=O" role="button" title="Agregar"><i class="ti-plus"></i> ALTA OFICIALES </a>
+<?php
+    } else if ($usu_04 == 39393 && $usu_05 == 9) {
+?>
+                                        <a class="btn btn-info" style="background-color:#005ea6; border-color:#005ea6;"  href="../examen/prencuentro_2_crud.php?competicion=<?php echo $valorCompeticion; ?>&encuentro=<?php echo $valorEncuentro; ?>&tipo=O" role="button" title="Agregar"><i class="ti-plus"></i> ALTA OFICIALES </a>
+                                        <a class="btn btn-info" style="background-color:#005ea6; border-color:#005ea6;"  href="../examen/prencuentro_2_crud.php?competicion=<?php echo $valorCompeticion; ?>&encuentro=<?php echo $valorEncuentro; ?>&tipo=T" role="button" title="Agregar"><i class="ti-plus"></i> ALTA DELEGADOS </a>
+                                        <a class="btn btn-info" style="background-color:#005ea6; border-color:#005ea6;"  href="../examen/prencuentro_2_crud.php?competicion=<?php echo $valorCompeticion; ?>&encuentro=<?php echo $valorEncuentro; ?>&tipo=P" role="button" title="Agregar"><i class="ti-plus"></i> ALTA JUGADORES </a>
+<?php
+    } else {
+?>
+                                        <a class="btn btn-info" style="background-color:#005ea6; border-color:#005ea6;"  href="../examen/preencuentro_2_crud.php?competicion=<?php echo $valorCompeticion; ?>&encuentro=<?php echo $valorEncuentro; ?>&tipo=T" role="button" title="Agregar"><i class="ti-plus"></i> ALTA DELEGADOS </a>
+                                        <a class="btn btn-info" style="background-color:#005ea6; border-color:#005ea6;"  href="../examen/preencuentro_2_crud.php?competicion=<?php echo $valorCompeticion; ?>&encuentro=<?php echo $valorEncuentro; ?>&tipo=P" role="button" title="Agregar"><i class="ti-plus"></i> ALTA JUGADORES </a>
+<?php
+    }
+?>
+                                	</h4>
+								</div>
+                                <div class="table-responsive">
+                                    <table id="tableLoad" class="table v-middle" style="width: 100%;">
+                                        <thead id="tableCodigo" class="<?php echo $usu_04; ?>">
+                                            <tr class="bg-conmebol">
+                                                <th class="border-top-0" style="text-align:center;" rowspan="2">C&Oacute;DIGO</th>
+                                                <th class="border-top-0" style="text-align:center;" colspan="2">TEST</th>
+                                                <th class="border-top-0" style="text-align:center;" rowspan="2">COMPETICI&Oacute;N</th>
+                                                <th class="border-top-0" style="text-align:center;" rowspan="2">ENCUENTRO</th>
+                                                <th class="border-top-0" style="text-align:center;" rowspan="2">EQUIPO</th>
+                                                <th class="border-top-0" style="text-align:center;" rowspan="2">JUGADOR</th>
+                                                <th class="border-top-0" style="text-align:center;" colspan="5">LABORATORIO</th>
+                                                <th class="border-top-0" style="text-align:center;" colspan="3">AUDITORIA</th>
+                                            </tr>
+                                            <tr class="bg-conmebol">
+                                                <th class="border-top-0" style="text-align:center;">ESTADO</th>
+                                                <th class="border-top-0" style="text-align:center;">FECHA</th>
+                                                <th class="border-top-0" style="text-align:center;">NOMBRE</th>
+                                                <th class="border-top-0" style="text-align:center;">FEC. ENVIO</th>
+                                                <th class="border-top-0" style="text-align:center;">FEC. RECEPCI&Oacute;N</th>
+                                                <th class="border-top-0" style="text-align:center;">POSITIVO</th>
+                                                <th class="border-top-0" style="text-align:center; width:80px;">&nbsp;</th>
+                                                <th class="border-top-0" style="text-align:center;">USUARIO</th>
+                                                <th class="border-top-0" style="text-align:center;">FECHA HORA</th>
+                                                <th class="border-top-0" style="text-align:center;">IP</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Modal Procesar -->
+                <div id="modaldiv" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="vcenter" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" id="modalcontent">
+                    </div>
+                </div>
+                <!-- Modal Procesar -->
+                <!-- ============================================================== -->
+                <!-- End PAge Content -->
+                <!-- ============================================================== -->
+                <!-- ============================================================== -->
+                <!-- Right sidebar -->
+                <!-- ============================================================== -->
+                <!-- .right-sidebar -->
+                <!-- ============================================================== -->
+                <!-- End Right sidebar -->
+                <!-- ============================================================== -->
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Container fluid  -->
+            <!-- ============================================================== -->
+<?php
+    include '../include/development.php';
+?>
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Page wrapper  -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <div class="chat-windows"></div>
+
+<?php
+    include '../include/footer.php';
+?>
+
+    <script src="../js/api.js"></script>
+    <script>
+        const _codPers = <?php echo $log_04; ?>;
+        const _codEqui = <?php echo $usu_04; ?>;
+        const _codComp = <?php echo $valorCompeticion; ?>;
+        const _codEncu = <?php echo $valorEncuentro; ?>;
+    </script>
+    <script src="../js/preencuentro.js"></script>
+</body>
+</html>
