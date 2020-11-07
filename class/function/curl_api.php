5406684282df57500c8f8f2af1a04c5f159b1d78
Christian Zelaya MACBOOK <christian@cerouno.com.py>
2020-11-07 09:41:48 -0300

    Ajuste 202011061525

--- class/function/curl_api.php
+++ class/function/curl_api.php
@@ -1,6 +1,9 @@
 <?php
-    $api = 'http://api.conmebol.com/portalmedico/public/v1';
-    $aut = 'dXNlcl9zZmhvbG94Om5zM3JfNWZoMCEweA==';
+    $api        = 'http://api.conmebol.com/portalmedico/public/v1';
+    $aut        = 'dXNlcl9zZmhvbG94Om5zM3JfNWZoMCEweA==';
+
+    $api_comet  = 'https://api-latam.analyticom.de/api/export/comet';
+    $aut_comet  = 'ZGllZ29nb256YWxlejpkaWVnb2dvbnphbGV6Q09O';
 
     function get_curl($ext){
         global $api;
@@ -83,6 +86,27 @@
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $result                     = curl_exec($ch);
         curl_close($ch);
+        return $result;
+    }
+
+    function get_curl_comet($ext){
+        global $api_comet;
+        global $aut_comet;
+        $urlAPI                     = $api_comet.'/'.$ext;
+        $ch                         = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $urlAPI);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 0);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept: application/json", "Authorization: Basic ".$aut_comet, "Content-Type: application/json"));
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result                     = curl_exec($ch);
+        curl_close($ch);
+        $result                     = json_decode($result, TRUE);
         return $result;
     }
 ?>
